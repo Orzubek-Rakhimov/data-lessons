8c03c9906ec6175b70d1a4472e31dda6c193b6b1
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-01-31 17:04:18 +0500

    8 dars
    Add main.py with example print statements and variables

---
+++ lessons/lesson-8/main.py
@@ -0,0 +1,22 @@
+# print() console ga malumot chiqaradi 
+
+# print("Salom Dunyo")
+
+x = 12 
+# print(x)
+
+farhod = "Farhod"
+# print(farhod)
+
+
+# yosh "yosh"  bu ikkisi br xil narsa emas 
+# yosh = 79
+# print("yosh")
+# print(yosh)
+
+print(x,farhod)
+
+
+
+
+
