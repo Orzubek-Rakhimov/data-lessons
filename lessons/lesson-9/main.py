dd2bf082ad80641f15ec40dab38db795dde103fd
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-03 17:55:52 +0500

    Implement variable declarations and type checks
    Added variables and type checks for different data types.

---
+++ lessons/lesson-9/main.py
@@ -0,0 +1,31 @@
+ism = "Farhod"
+y = "Doner Kebab"
+meva = "Olma yegim keldi"
+
+# print(type(ism))
+# print(type(y))
+
+
+
+birYildakiSekundalar = 31_536_000 
+age = 13
+odamSoni = 15
+sinfdakiOdamlarSoni = 8
+
+# print(type(birYildakiSekundalar))
+# print(type(odamSoni))
+
+
+floatgaMisol = 10.2
+floatgaMisol2 = 9.9
+floatgaMisol3 = 12.0
+
+# print(type(floatgaMisol))
+# print(type(floatgaMisol3))
+
+
+buleanTogri = True
+buleanNotogri = False
+
+# print(type(buleanNotogri))
+print(type(buleanNotogri))
