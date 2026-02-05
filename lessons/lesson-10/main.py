1c097e6671ef5557eb4947998c87fcff73e56f39
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-05 18:39:05 +0500

    Create main.py with data type examples
    Add main.py with examples of data types and input handling.

---
+++ lessons/lesson-10/main.py
@@ -0,0 +1,48 @@
+# str()
+# int()
+# float()
+# bool()
+
+# input()
+
+# input("Yoshingizni kiriting: ")
+
+# yosh = input("Yoshingizni kiriting: ")
+
+# print("Sizning yoshingiz: ",yosh,"da")
+
+
+# son1 = int(input("Birinchi sonni kiriting:"))
+# son2 = int(input("Ikkinchi sonni kiriting:"))
+
+
+# type()
+# print(type(son1))
+# print(type(son2))
+
+# print(son1 + son2)
+
+# Xulosa input str tipindaki malumotlarni qaytaradi
+
+
+# print(int("on ikki"))
+
+
+# print(float(2))
+# print(type(float("2.23")))
+
+# print(type(str(10)))
+
+# print(type(bool(1)))
+
+
+# print(int(12.6))
+
+
+# x = float(12)
+# print(x , type(x))
+
+# y = float("134.34")
+# print(y , type(y))
+
+
