7bd47ab49d53c1ccbf7e01725a68cd0f438f4c23
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-07 18:33:57 +0500

    Add pizza price calculation script

---
+++ lessons/lesson-11/main.py
@@ -0,0 +1,31 @@
+# print(100 + 122)
+
+# print(57 - 23)
+
+# print(10 / 5)
+
+# print(134 * 126)
+
+
+# print (100 // 3)  # 33 (1 qoldiq)
+
+# print ( 71 // 2) # 35 (1 qoldiq)
+
+
+# print( 100 % 3)
+
+# print ( 71 % 2) 
+
+
+# print ( 5**2 )
+
+# print ( 5 ** 3 )
+
+
+
+pitsaNarxi = float(input("Pitsani narxini kiriting: "))
+pitsaSoni = int(input("Pitsani sonini kiriting: "))
+odamSoni = int(input("Odam Soni:"))
+
+print((pitsaSoni * pitsaNarxi) // odamSoni)
+
