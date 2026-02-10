380485715a0ccff1c0070dcd653289717f16b538
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-10 18:13:08 +0500

    Create main.py for lesson 12 on comparison operators
    Add lesson 12 demonstrating comparison operators in Python.

---
+++ lessons/lesson-12/main.py
@@ -0,0 +1,54 @@
+
+# ( == ) teng Ture/False
+
+# print(2==2)
+# print(3==5)
+# print (17 == -17)
+
+# print (5-3 == 17-15)
+
+
+# ( != )
+
+# print(3 != 5)
+
+# print ( 12 != 12 )
+
+# print ( 7-3 != 16+2) #@ => 4 != 18
+
+
+# ( > )
+
+# print( 10 > 5)
+# print ( 27 - 10 < 14 + 6) # => 17 < 20
+
+# ( < )
+
+# print( 278 < 278.001 ) 
+# print ( 300 < -300 )
+
+
+# ( >= )
+
+# print ( 2 >= 2)
+# print ( -3 >= -3.5)
+
+
+# ( <= )
+
+# print ( 17 <= 17)
+# print ( -200 <= -200.001)
+
+
+
+# son = int(input("Sonni kiriting:"))
+# qoldiq = son % 2
+# print(qoldiq == 0)
+
+# print(int(input("Sonni kiriting:")) % 2 == 0)
+
+
+# x = 10 < 3
+
+# print(x == True)
+
