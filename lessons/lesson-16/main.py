1fd9c851f6f79d23858e75af31d18465284bde55
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-19 18:33:56 +0500

    Add main.py with various loop examples

---
+++ lessons/lesson-16/main.py
@@ -0,0 +1,89 @@
+x = 1
+
+# x = x + 1 1ga oshiradi 2 -> 3 , 3 -> 4
+# x = x - 1 1ga kamaytirad 3 -> 2 , 4 -> 3
+
+
+# 1 < 10 => True
+# 2 < 10 => True 
+# 3 < 10 => True
+# 9 < 10 => True
+# 10 < 10 => False
+# 11 < 11 => False
+
+# 1
+# while x < 11 : 
+#     # 9
+#     print(x)
+#     # 10
+#     x=x+1
+     
+# y = 10
+
+# 2
+# while y > 0 :
+#     print(y)
+#     y = y - 1
+
+
+# x = 2 
+
+# while x < 101:
+#     print(x)
+#     x = x + 2
+     
+# x = 1
+
+# while x < 101:
+#     if x % 2 == 0:
+#         print(x)
+#     x = x + 1     
+
+
+
+# son = int(input("Sonni kiriting"))
+
+# while son >= 0:
+#     print(son)
+#     son = son - 1
+
+
+# son = int(input("Sonni kiriting"))
+
+# while son > - 1:
+#     print(son)
+#     son = son - 1
+
+
+# 12345abd
+# asdasd
+# asdasdsad
+# 12345abd
+# print kod to'gri
+
+
+# while True:
+#     kod = input("Kodni kiriting:")
+#     if kod == "12345abd":
+#         print("Kod to'gri")
+#     else:
+#         print("Kod noto'gri")
+
+
+# kod = input("Kodni kiriting: ")
+
+# while kod != "12345abd":
+#     kod = input("kodni kiriting: ")
+    
+# print("Kod to'gri")
+
+
+# pul = float(input("Pulni kirgazing: "))
+
+# while pul > 0 :
+#     print(pul)
+#     pul = pul - 10
+#     if pul <= 0 :
+#         print(0)
+#         print("pul tugadi")
+
