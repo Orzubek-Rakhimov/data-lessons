5e98a94fe6083e57898b7672231c22d768d08f5b
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-19 18:34:33 +0500

    Add main.py for lesson 15 with various examples

---
+++ lessons/lesson-15/main.py
@@ -0,0 +1,76 @@
+# son1=float(input("1-Sonni kiriting :"))
+# son2=float(input("2-Sonni kiriting :"))
+# son3=float(input("3-Sonni kiriting :"))
+
+# if son1>son2 and son1>son3:
+#  print(son1, "Eng katta son")
+# elif son2>son1 and son2> son3:
+#     print(son2, "Eng katta son")
+# elif son3>son1 and son3>son2:
+#     print(son3, "Eng katta son")
+# elif son1==son2 and son2==son3:
+#     print("Xamma son teng !")
+
+
+# 10
+# 10
+# 10
+
+
+# 10
+# 10
+# 9
+
+# 9
+# 10
+# 10
+
+# 10
+# 9
+# 10
+
+# 1 va 2 sonlar ebg kattasi : 10
+
+# if son1==son2 and son1==son3:
+#     print("Xamma son teng !")
+# elif son1>son2 and son1>son3:
+#     print("Eng katta son1 :",son1)
+# elif son2>son1 and son2>son3:
+#     print("Eng katta son2 :",son2)
+# elif son1 == son2 and son1 > son3:
+#     print("son1 va son2 eng katta son :" , son1)
+# elif son2 == son3 and son1 > son3:
+#     print("son2 va son3 eng katta son :" , son2)
+# elif son1 == son3 and son1 > son2:
+#     print("son1 va son3 eng katta son :" , son3)
+# else:
+#      print("Eng katta son3 :",son3)    
+
+
+
+# baxo=float(input("baho kiriting: "))
+# if baxo>=90 and baxo<=100:
+#     print("sizning bahoingiz 5")
+# elif baxo>=80 and baxo<90:
+#     print("sizning bahoingiz 4") 
+# elif baxo>=70 and baxo<80:
+#     print("sizning bahoingiz 3")
+# elif baxo>=60 and baxo<70:
+#     print("sizning bahoingiz 2")
+# elif baxo>=0 and baxo<60:
+#     print("sizning bahoingiz 1")
+# else:
+#     print("baho 0 dan 100 gacha bo'lishi kerak")  
+    
+
+# l = input("Loginni kiriting: ")
+# p = float(input("Parolni kiriting: ")) # 55555.0
+
+# if l == "HELLO" and p == 55555:
+#     print (" Kirish mumkin")
+# else:
+#     print (" Kirish mumkin emas")
+
+
+
+    
