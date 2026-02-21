7f1387a5aa736150be40a7ff918f550f8442b33e
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-21 18:18:23 +0500

    Add commented out examples of loops and conditions

---
+++ lessons/lesson-17/main.py
@@ -0,0 +1,51 @@
+# n = 1
+
+# while n <= 10:
+#     print(n)
+#     if n == 5:
+#         break
+#     n=n+1
+#     print("keyin" , n)
+
+
+
+# n = 0
+
+# while n < 10:
+#     n=n+1
+#     if n == 6:
+#         continue 
+#     print(n)
+ 
+ 
+
+# while True:
+#     inp = input("Qiymat kiriting: ")
+#     if inp == "exit":
+#         break
+    
+    
+    
+# n = 0
+
+# while n < 20 :
+#     n=n+1
+#     if n % 2 == 1:
+#         continue
+#     print(n)
+    
+    
+# son = 2
+# while son <= 20:
+#     print(son)
+#     son=son+2
+
+
+# while True:
+#     parol = input("Parolni kiriting ")
+#     if parol == "python123":
+#         print("Xush kelisiz!")
+#         break
+#     elif parol == "cancel":
+#         print("Dastur bekor qilindi!")
+#         break
